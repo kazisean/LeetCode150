9d5d1dca3e86e11cb733581453290e906a4241d6
Kazi Hossain <keh8423@nyu.edu>
2024-06-27 17:54:55 -0400

    save

---
+++ E26.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class E26 {
+
+    public static void main(String[] args) {
+        int arr [] = {1,1,2};
+        System.out.println();
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int p = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            
+        }
+
+        return p;
+    }
+
+
+
+    
+}
--- test.java
+++ test.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class test {
 
     public static void main(String[] args) {
+        int arr [] = {};
         System.out.println("Hello");
-
 
     }
 
